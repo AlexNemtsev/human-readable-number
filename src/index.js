648e29547a6980f9d377615d98c1ba8eababd6c1
Alexander Nemtsev <mrcrazy696@gmail.com>
2022-06-21 07:40:56 +0300

    feat: count up to 99

--- src/index.js
+++ src/index.js
@@ -1,34 +1,45 @@
-module.exports = function toReadable (number) {
-  const nineteen = {
-    1: 'one',
-    2: 'two',
-    3: 'three',
-    4: 'four',
-    5: 'five',
-    6: 'six',
-    7: 'seven',
-    8: 'eight',
-    9: 'nine',
-    10: 'ten',
-    11: 'eleven',
-    12: 'twelve',
-    13: 'thirteen',
-    14: 'fourteen',
-    15: 'fifteen',
-    16: 'sixteen',
-    17: 'seventeen',
-    18: 'eighteen',
-    19: 'nineteen'
-  }
+module.exports = function toReadable(number) {
+    const twenty = {
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+    }
 
-  const tens = {
-    20: 'twenty',
-    30: 'thirty',
-    40: 'fourty',
-    50: 'fifty',
-    60: 'sixty',
-    70: 'seventy',
-    80: 'eighty',
-    90: 'ninety'
-  }
+    const tens = {
+        30: 'thirty',
+        40: 'fourty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety'
+    }
+
+    if (number <= 20) {
+        return twenty[number];
+    } else if (number < 100) {
+        if (number % 10 === 0) {
+            return tens[number];
+        } else {
+            const residual = number % 10;
+            return tens[number - residual] + ' ' + twenty[residual]
+        }
+    }
 }
